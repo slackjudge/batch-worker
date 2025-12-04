3196b36043af83980f6939739259964b8b42b8fa
tkv00 <tkv0098@gmail.com>
2025-12-04 15:36:38 +0900

    init: create spring application

---
+++ slackjudge/src/main/java/store/slackjudge/batch/SlackjudgeApplication.java
@@ -0,0 +1,13 @@
+package store.slackjudge.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackjudgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackjudgeApplication.class, args);
+	}
+
+}
---
+++ slackjudge/src/test/java/store/slackjudge/batch/SlackjudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package store.slackjudge.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackjudgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
