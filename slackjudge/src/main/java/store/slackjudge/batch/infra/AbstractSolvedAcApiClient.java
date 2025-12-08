121ce46b240df6683574de97d20878059375e1d7
tkv00 <tkv0098@gmail.com>
2025-12-08 15:00:06 +0900

    fix/#8: 공통 로직 개별 로직 분리

--- slackjudge/src/main/java/store/slackjudge/batch/infra/AbstractSolvedAcApiClient.java
+++ slackjudge/src/main/java/store/slackjudge/batch/infra/AbstractSolvedAcApiClient.java
@@ -5,16 +5,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
 @Slf4j
+
 public abstract class AbstractSolvedAcApiClient <T>{
     //재시도 횟수
     private final int RETRY=5;
 
+
     private final WebClient webClient;
+    private final UrlBuilder urlBuilder;
+
     //생성자
-    protected AbstractSolvedAcApiClient(WebClient webClient){
+    protected AbstractSolvedAcApiClient(WebClient webClient,UrlBuilder urlBuilder){
         this.webClient=webClient;
+        this.urlBuilder=urlBuilder;
     }
     //API별 요청 파라미터 생성
     protected abstract Map<String,String> createRequestParameter(String bojId);
@@ -23,18 +29,59 @@ public abstract class AbstractSolvedAcApiClient <T>{
     protected abstract String setUpUrl();
 
     //API별 응답 파싱
-    protected abstract T parseResponse(String body);
+    protected abstract T parseResponse(String response);
 
     //API별 에러 처리
-    protected abstract void handleError();
+    protected abstract void handleError(Exception e);
 
+    //API 호출
+    protected String request(String url){
+        return webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    //재시도
+    protected <R> R retry(Supplier<R> supplier){
+        int attempt=0; //시도 횟수
+
+        while(true){
+            try {
+                return supplier.get();
+            }catch (Exception e){
+                attempt++;
+
+                if (attempt>=RETRY) {
+                    log.error("[calling solved.ac API] retry all failed");//재시도 모두 실패
+                    throw e;
+                }
+
+                log.warn("[calling solved.ac API] request failed attempt = {}/{}",attempt,RETRY);
+            }
+        }
+    }
     /**
      * =====================
      *        공통 로직
      * =====================
      */
-    public final T call(){
-        String url=setUpUrl()
+
+    //요청
+    public final T call(String bojId){
+        String baseUrl=setUpUrl();
+        Map<String,String> params=createRequestParameter(bojId);
+        String url=urlBuilder.buildUrl(baseUrl,params);
+
+        log.debug("[calling solved.ac API] url : {}",url);
+
+        try {
+            return retry(()->parseResponse(request(url)));
+        }catch (Exception e){
+            handleError(e);
+            throw e;
+        }
     }
 
 }
