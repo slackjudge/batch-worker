73202a7a68d398e4c6b1b270f14dab24bcc4a694
tkv00 <tkv0098@gmail.com>
2025-12-08 14:12:19 +0900

    feat/#8: solved.ac properties setup

---
+++ slackjudge/src/main/java/store/slackjudge/batch/infra/SolvedAcProperties.java
@@ -0,0 +1,21 @@
+package store.slackjudge.batch.infra;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "solved")
+public class SolvedAcProperties {
+    private Api api;
+
+    @Getter
+    @Setter
+    public static class Api{
+        private String userInfo;
+        private String problemInfo;
+    }
+}
