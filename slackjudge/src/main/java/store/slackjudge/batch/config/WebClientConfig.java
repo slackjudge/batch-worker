95d5df32e1bd2d45284300020f33cac9d6e3244b
tkv00 <tkv0098@gmail.com>
2025-12-09 17:34:00 +0900

    fix/#12: webClient 응답 timeout 설정

--- slackjudge/src/main/java/store/slackjudge/batch/config/WebClientConfig.java
+++ slackjudge/src/main/java/store/slackjudge/batch/config/WebClientConfig.java
@@ -2,12 +2,21 @@ package store.slackjudge.batch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    public WebClient webClient() {
+        return WebClient
+                .builder()
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create().responseTimeout(Duration.ofSeconds(10))
+                ))
+                .build();
     }
 }
