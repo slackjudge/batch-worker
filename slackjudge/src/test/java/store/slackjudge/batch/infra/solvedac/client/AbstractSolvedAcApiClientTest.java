6f042eaea5c6c8bfda65051a86ecaaba0d7a90e0
tkv00 <tkv0098@gmail.com>
2025-12-09 14:35:32 +0900

    test/#11: retry 테스트 Nested 분리

--- slackjudge/src/test/java/store/slackjudge/batch/infra/solvedac/client/AbstractSolvedAcApiClientTest.java
+++ slackjudge/src/test/java/store/slackjudge/batch/infra/solvedac/client/AbstractSolvedAcApiClientTest.java
@@ -3,6 +3,7 @@ package store.slackjudge.batch.infra.solvedac.client;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -32,15 +33,15 @@ class AbstractSolvedAcApiClientTest {
      * test용 stub 객체
      */
     @BeforeEach
-    void setUp(){
-        client=new AbstractSolvedAcApiClient<String>(webClient,urlBuilder) {
+    void setUp() {
+        client = new AbstractSolvedAcApiClient<String>(webClient, urlBuilder) {
             @Override
             protected Map<String, String> createRequestParameter(String bojId, int page) {
-                bojId="test";
-                page=3;
-                Map<String,String> params=new HashMap<>();
-                params.put("query",bojId);
-                params.put("page",String.valueOf(page));
+                bojId = "test";
+                page = 3;
+                Map<String, String> params = new HashMap<>();
+                params.put("query", bojId);
+                params.put("page", String.valueOf(page));
 
                 return params;
             }
@@ -61,65 +62,71 @@ class AbstractSolvedAcApiClientTest {
             }
         };
     }
+
     @Mock
     private WebClient webClient;
 
     @Mock
     private UrlBuilder urlBuilder;
 
-    private static final String FAIL="FAIL";
-    private static final String SUCCESS="SUCCESS";
+    private static final String FAIL = "FAIL";
+    private static final String SUCCESS = "SUCCESS";
 
 
-    @Test
-    @DisplayName("처음 성공 시 결과 값 반환")
-    void retry_First_Success(){
-        //given
-        AtomicInteger attempt=new AtomicInteger(0);
-        Supplier<String> supplier=()->{
-            attempt.incrementAndGet();
-            return SUCCESS;
-        };
+    @Nested
+    @DisplayName("retry 동작 테스트")
+    class retryTest {
+        @Test
+        @DisplayName("처음 성공 시 결과 값 반환")
+        void retry_First_Success() {
+            //given
+            AtomicInteger attempt = new AtomicInteger(0);
+            Supplier<String> supplier = () -> {
+                attempt.incrementAndGet();
+                return SUCCESS;
+            };
 
-        //when
-        String result= client.retry(supplier).toString();
+            //when
+            String result = client.retry(supplier).toString();
 
-        //then
-        assertThat(result).isEqualTo(SUCCESS);
-        assertThat(attempt.get()).isEqualTo(1);
-    }
+            //then
+            assertThat(result).isEqualTo(SUCCESS);
+            assertThat(attempt.get()).isEqualTo(1);
+        }
 
-    @Test
-    @DisplayName("3번 실패 후 성공하면 결과 반환")
-    void retry_ThirdAttemptNextSuccess(){
-        //given
-        AtomicInteger attempt=new AtomicInteger(0);
-        Supplier<String> supplier=()->{
-            if (attempt.incrementAndGet()<3){
+        @Test
+        @DisplayName("3번 실패 후 성공하면 결과 반환")
+        void retry_ThirdAttemptNextSuccess() {
+            //given
+            AtomicInteger attempt = new AtomicInteger(0);
+            Supplier<String> supplier = () -> {
+                if (attempt.incrementAndGet() < 3) {
+                    throw new RuntimeException(FAIL);
+                }
+                return SUCCESS;
+            };
+
+            //when
+            String result = client.retry(supplier);
+
+            //then
+            assertThat(result).isEqualTo(SUCCESS);
+            assertThat(attempt.get()).isEqualTo(3);
+        }
+
+        @Test
+        @DisplayName("5회 모두 실패하면 예외 발생")
+        void retry_AllFailed() {
+            //given
+            Supplier<String> supplier = () -> {
                 throw new RuntimeException(FAIL);
-            }
-            return SUCCESS;
-        };
+            };
 
-        //when
-        String result=client.retry(supplier);
-
-        //then
-        assertThat(result).isEqualTo(SUCCESS);
-        assertThat(attempt.get()).isEqualTo(3);
+            //when & then
+            assertThatThrownBy(() -> client.retry(supplier))
+                    .isInstanceOf(RuntimeException.class)
+                    .hasMessage(FAIL);
+        }
     }
 
-    @Test
-    @DisplayName("5회 모두 실패하면 예외 발생")
-    void retry_AllFailed(){
-        //given
-        Supplier<String> supplier=()->{
-            throw new RuntimeException(FAIL);
-        };
-
-        //when & then
-        assertThatThrownBy(()-> client.retry(supplier))
-                .isInstanceOf(RuntimeException.class)
-                .hasMessage(FAIL);
-    }
 }
