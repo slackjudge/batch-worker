62c3763365cc97ea20af9fd4e721c180e35ce02d
tkv00 <tkv0098@gmail.com>
2025-12-10 17:40:30 +0900

    chore/#17: problem detector method별 주석 추가

--- slackjudge/src/main/java/store/slackjudge/batch/infra/mongo/service/detector/ProblemChangeDetector.java
+++ slackjudge/src/main/java/store/slackjudge/batch/infra/mongo/service/detector/ProblemChangeDetector.java
@@ -21,11 +21,33 @@ import java.util.stream.Collectors;
 public class ProblemChangeDetector implements SnapshotDetectStrategy<ProblemSearchResponse> {
     private final ProblemJdbcRepository repository;
 
+    /*==========================
+    *
+    *ProblemChangeDetector
+    * solved.ac API 통신으로 최신 유저가 푼 문제 정보와 1시간 전 유저가 푼 문제 정보 비교 메서드
+    * @parm context:변경감지 객체
+    * @return true:변경 o / false:변경 x
+    * @author kimdoyeon
+    * @version 1.0.0
+    * @date 25. 12. 10.
+    *
+    ==========================**/
     @Override
     public boolean detect(DetectionContext<ProblemSearchResponse> context) {
         return context.previous().getSolvedCount() != context.current().count();
     }
 
+    /*==========================
+    *
+    *ProblemChangeDetector
+    * RDB에 유저의 새로 푼 문제 업데이트
+    * @parm context:변경감지 객체
+    * @return void
+    * @author kimdoyeon
+    * @version 1.0.0
+    * @date 25. 12. 10.
+    *
+    ==========================**/
     @Override
     public void update(DetectionContext<ProblemSearchResponse> context) {
         //스냅샷에 존재하는 유저의 모든 문제 번호
