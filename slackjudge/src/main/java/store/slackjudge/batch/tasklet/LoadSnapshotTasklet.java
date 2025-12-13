69152808fae39cb1f5ec8f1143f0acea2f70cba0
tkv00 <tkv0098@gmail.com>
2025-12-13 18:14:30 +0900

    fix/#18: @StepScope 애노테이션 추가

--- slackjudge/src/main/java/store/slackjudge/batch/tasklet/LoadSnapshotTasklet.java
+++ slackjudge/src/main/java/store/slackjudge/batch/tasklet/LoadSnapshotTasklet.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ExecutionContext;
@@ -34,7 +35,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 
 @Component
-
+@StepScope
 public class LoadSnapshotTasklet implements Tasklet {
     /**
      * next step : FetchTierAndCountTasklet
